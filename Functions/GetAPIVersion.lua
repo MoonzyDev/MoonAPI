5dcca12b5c1408b07c5b69f21cc3e11861505e96
Moonzy <moonzy@cock.email>
2024-12-17 19:54:26 +0000

    GetAPIVersion Fix
    [/]: Fixed Public & Not-Aligned Snapshots returning "Dependencies/Build" instead of "Dependencies/Version"
    [/]: Fixed Nightly Snapshots returning "Dependencies/NightlyBuild" instead of "Dependencies/NightlyVersion"

--- Functions/GetAPIVersion.lua
+++ Functions/GetAPIVersion.lua
@@ -1,7 +1,7 @@
 return function()
     if getgenv().MAPI.SNAPSHOT == "Public" or getgenv().MAPI.SNAPSHOT == nil then
-        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Build");
+        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Version");
     elseif getgenv().MAPI.SNAPSHOT == "Nightly" then
-        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/NightlyBuild");
+        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/NightlyVersion");
     else warn("[❌] [ＭＡＰＩ] Unknown version has been given in MAPI.SNAPSHOT. (check the documentation for help!)") end
 end
