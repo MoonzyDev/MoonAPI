aac619edf207cab31858c097cbcdc99d3106f950
Moonzy <moonzy@cock.email>
2024-12-17 19:22:27 +0000

    GetAPIVersion Release

---
+++ Functions/GetAPIVersion.lua
@@ -0,0 +1,7 @@
+return function()
+    if getgenv().MAPI.SNAPSHOT == "Public" or getgenv().MAPI.SNAPSHOT == nil then
+        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Build");
+    elseif getgenv().MAPI.SNAPSHOT == "Nightly" then
+        return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/NightlyBuild");
+    else warn("[❌] [ＭＡＰＩ] Unknown version has been given in MAPI.SNAPSHOT. (check the documentation for help!)") end
+end
