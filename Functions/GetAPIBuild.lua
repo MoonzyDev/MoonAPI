8b4510e15479ac8d231cb995c158f8af6fd0aabd
Moonzy <moonzy@cock.email>
2024-12-22 18:04:33 +0000

    Update GetAPIBuild.lua

--- Functions/GetAPIBuild.lua
+++ Functions/GetAPIBuild.lua
@@ -1,5 +1,5 @@
 return function()
-    if getgenv().MAPI.SNAPSHOT == "Public" or getgenv().MAPI.SNAPSHOT == nil then
+    if getgenv().MAPI.SNAPSHOT == nil or getgenv().MAPI.SNAPSHOT == "Public" then
         return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Build");
     elseif getgenv().MAPI.SNAPSHOT == "Nightly" then
         return game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/NightlyBuild");
