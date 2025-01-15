f6771a77cc93fe741e9ada31137ccb4216b1bdb2
Moonzy <moonzy@cock.email>
2025-01-15 23:10:40 +0000

    Updated for Beta 1001 (again)
    [accidentally posted the wrong version of the loader, my bad!]
    
    Made the speeds drastically faster by using multiple threads instead of a single thread to load modules. (0.5s avg for 1 module -> 0.05s avg per 1 module)
    Added "Time Taken" to the output by calculating the difference between a start tick and an end tick.
    
    Updated on 16/01/2025

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local Version = game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Version")
 local Build = game:HttpGet("https://raw.githubusercontent.com/MoonzyDev/MoonAPI/refs/heads/main/Dependencies/Build")
 local HttpService = game:GetService("HttpService")
-local MAPI_FUNCTIONS = {}
+local MAPI_FUNCTIONS = {};
+local completed = false;
 
 function init()
     local sT = os.time()
@@ -37,35 +38,44 @@ function init()
     end
 
     print(string.format('[⏹] Loading %s functions from "MAPI.Functions"', fA)) --> Print the amount of functions before loading any.
-
-    for _, file in ipairs(files) do
+    local lF = 0
+    for component, file in ipairs(files) do
         if file.type == "file" then
-            local fileContent
-            local contentSuc, contentErr = pcall(function()
-                fileContent = game:HttpGet(file.download_url)
+            task.spawn(function()
+                local fileContent
+                local contentSuc, contentErr = pcall(function()
+                    fileContent = game:HttpGet(file.download_url)
+                end)
+
+                if not contentSuc then
+                    warn("[❌] [ＭＡＰＩ] Error fetching file contents.")
+                    warn("  ➡ Report in our Discord Server: discord.gg/example.")
+                    warn("  ➡ Error: " .. contentErr)
+                    return
+                end
+
+                local func = loadstring(fileContent)
+                if func then
+                    MAPI_FUNCTIONS[file.name:sub(1, -5)] = func() --> removes .lua extension from the file
+                    print(string.format("[✅] Loaded function %s", file.name:sub(1, -5))) --> success
+                else
+                    warn(string.format("[❌] Failed to load function %s", file.name:sub(1, -5))) --> failure
+                end
+
+                lF += 1
+                if lF == fA then completed = not completed end;
             end)
-
-            if not contentSuc then
-                warn("[❌] [ＭＡＰＩ] Error fetching file contents.")
-                warn("  ➡ Report in our Discord Server: discord.gg/example.")
-                warn("  ➡ Error: " .. err)
-                continue
-            end
-
-            local func = loadstring(fileContent)
-            if func then
-                MAPI_FUNCTIONS[file.name:sub(1, -5)] = func() --> removes .lua extension from the file
-                print(string.format("[✅] Loaded function %s", file.name:sub(1, -5))) --> success
-            else
-                warn(string.format("[❌] Failed to load function %s", file.name:sub(1, -5))) --> failure
-            end
         end
     end
 
-    local diff = os.time() - sT
-    print(string.format("[⏹] Time Taken: %s", diff));
+    repeat task.wait(.05) until completed
+
+    local eT = os.time();
+    local diff = eT - sT;
+
     print(string.format("[⏹] Version %s", Version));
     print(string.format("[⏹] Moon API | %s Build", Build));
+    print(string.format("[⏹] Time Taken: %ss",diff));
     print("[⏹] Made by MoonzyDev (moonzy.dev)");
 end
 
